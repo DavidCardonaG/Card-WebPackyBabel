d56f0de93d99bdd5f1722b7c2966418a617e1f69
DavidCardonaG <softwarewd@outlook.com>
2021-08-27 12:10:42 -0500

    Estructura final webpack y babel

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-console.log('Hola Mundo');
+import Comentario from '../app/comentario.js';
+import '../app/styles.css';
+
+async function inicio(){
+    const com = new Comentario();
+    const res = await com.obtenerCategorias()
+    console.log(res.categories)
+
+}
+
+inicio()
+
